eec5f168ad581e06b67d5b775643dfeb2c18ab58
Aakhib Shaik <114942516+skAakhi@users.noreply.github.com>
2025-01-23 15:51:59 +0530

    Update Main.java

--- 22-01-25/Task/Main.java
+++ 22-01-25/Task/Main.java
@@ -7,11 +7,10 @@ public static void main(String[] args)
 Scanner sc = new Scanner(System.in);
 Logic i = new Logic();
 boolean flag=true;
-
+try{
 while(flag)
 {
 System.out.printf("\n 1 - To ADD\n 2- To deleteByIndex\n 3- To delete all elements\n 4- Fetch current elements\n 5- To exit : \n");
-try{
 int opt = sc.nextInt();
 switch(opt)
 {
@@ -27,10 +26,12 @@ break;
 case 4 : i.fetch();
 break;
 case 5 : 
+i.closeResource();
 System.out.println("Code exits...Thank you ! ");
 flag=false;
 break;
 default : throw new InvalidChoiceException("Wrong Option please try again ! ");
+}
 }
 }
 catch(InvalidChoiceException e)
@@ -41,7 +42,9 @@ catch(IndexOutOfBoundsException e)
 {
 System.out.println(e.getMessage());
 }
+finally{
+sc.close();
+}
 }
 }
 
-}
